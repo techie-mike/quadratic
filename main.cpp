540260618ae44dc72bd34a5da62df3e76a5504a2
Tekhnar <kaskovm2014@ya.ru>
2019-09-29 11:08:25 +0300

    Add define on UnitTest

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <math.h>
 #include <assert.h>
 
-//--switching-on UnitTest--//
-const int switchTEST = 1;
-//  If "0" - OFF
-//  If "1" - ON
+//--uncomment this #define if want turn on UnitTest--//
+
+#define switchTEST
+
 //-------------------------//
 
 const int NORESULT   = 0;
@@ -21,20 +21,25 @@ int SolveSquare(double a, double b, double c, double * x1, double *x2);
 int SolveLinearA(double b, double c, double *x1);
 void PrintResults(int sum, double x1, double x2);
 void UnitTest();
-int IsZero(double);
-double *Input_calloc(const char text[], int amount);
+int IsZero(double value, double full_error);
+double *InputCalloc(const char text[], int amount);
+int HowPow(double x);
+double MaxError(double a, double b, double c);
+
 
 int main()
 {
     double x1 = NAN, x2 = NAN;
     printf("Program for solving the quadratic equation.\n");
     printf("Michail Kaskov 05.09.2019\n");
-    if (switchTEST){
+
+    #ifdef switchTEST
         UnitTest();
         return 0;
-    }
-    double *data = Input_calloc("Input a, b, c:", 3);
-    assert(data!=0);
+    #endif
+
+    double *data = InputCalloc("Input a, b, c:", 3);
+    assert(data != 0);
     int sum = SolveSquare(data[0], data[1], data[2], &x1, &x2);
     free(data);
     PrintResults(sum, x1, x2);
@@ -50,10 +55,14 @@ int main()
  * \return double* pointer on array of variables
  *
  */
-double *Input_calloc(const char text[], const int amount){
+double *InputCalloc(const char text[], const int amount){
+    assert(text != 0);
+    assert(amount > 0);
+
     printf(text);
     double *data = (double*) calloc(amount, sizeof(data[0]));
-    for (int i=0; i< amount; i++){
+    assert(data != 0);
+    for (int i = 0; i < amount; i++){
         if (!scanf("%lg", &data[i])) {
             i--;
             fflush(stdin);
@@ -63,6 +72,45 @@ double *Input_calloc(const char text[], const int amount){
     return data;
 }
 
+double MaxError(double a, double b, double c){
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
+
+    int max_pow = HowPow(a);
+
+    int temp = HowPow(b);
+    if (max_pow < temp){
+        max_pow = temp;
+    }
+
+    temp = HowPow(c);
+    if (max_pow < temp){
+        max_pow = temp;
+    }
+    max_pow = pow(10, max_pow);
+    return 1e-10 * max_pow;
+}
+
+int HowPow(double x){
+    int num_pow = 0;
+    x = abs(x);
+    if (x >= 10){
+        while (x >= 10){
+            num_pow++;
+            x /= 10;
+        }
+    }
+
+    if (x < 1){
+        while (x < 1){
+            num_pow--;
+            x *= 10;
+        }
+    }
+
+    return num_pow;
+}
 
 /** \brief quadratic solution
  *
@@ -79,21 +127,25 @@ int SolveSquare(double a, double b, double c, double *x1, double *x2){
     // a*x^2 + b*x + c = 0 //
     /////////////////////////
 
-    if (IsZero(a)) return SolveLinearA(b, c, x1);
-    if (IsZero(b)) {
-        if (c==0){
+    double full_error = MaxError(a,b,c);
+    assert(isfinite(full_error));
+    //printf("full_error = %lg", full_error);
+
+    if (IsZero(a, full_error)) return SolveLinearA(b, c, x1);
+    if (IsZero(b, full_error)) {
+        if (c == 0){
             *x1 = 0;
             return ONERESULT;
             }
             else{
                 //printf("%lg %lg", c, a);
                 *x1 = sqrt(-c/a);
-                *x2 =- *x1;
+                *x2 = -*x1;
                 assert(isfinite(*x1));
             return TWORESULT;
         }
     }
-    if (IsZero(c)){
+    if (IsZero(c, full_error)){
         *x1 = 0;
         SolveLinearA(a, b, x2);
         assert(isfinite(*x2));
@@ -104,7 +156,7 @@ int SolveSquare(double a, double b, double c, double *x1, double *x2){
 
     if (d < 0) return NORESULT;
     if (d == 0) {
-        *x1=-b/(2*a);
+        *x1 = -b/(2*a);
         return ONERESULT;
     }
     d = sqrt(d);
@@ -167,13 +219,12 @@ void PrintResults(int sum, double x1, double x2){
  * \return int TRUE if "zero" else FALSE
  *
  */
-int IsZero(double value){
-    double zero = value * 1e-10;
-    if (fabs(value) <= zero) return TRUE;
+int IsZero(double value, double full_error){
+    if (fabs(value) <= full_error) return TRUE;
     else return FALSE;
 }
 
-
+#ifdef switchTEST
 void UnitTest(){
     printf("TEST\n");
     const int nTest = 4;
@@ -186,7 +237,7 @@ void UnitTest(){
         double c;
         double x1;
         double x2;
-        double sum;
+        int sum;
 
     } arrayTEST[nTest] = {
         1,  6,  9, -3,  0, 1,
@@ -197,37 +248,35 @@ void UnitTest(){
 
     for (int i = 0; i<nTest; i++){
         double x1 = NAN, x2 = NAN;
-        double *data = (double*) calloc(3, sizeof(data[0]));
 
-        data[0] = arrayTEST[i].a;
-        data[1] = arrayTEST[i].b;
-        data[2] = arrayTEST[i].c;
-
-        int sum = SolveSquare(data[0], data[1], data[2], &x1, &x2);
-        free(data);
+        double full_error = 1e-10; //MaxError(arrayTEST[i].a, arrayTEST[i].b, arrayTEST[i].c);
+        assert(isfinite(full_error));
+        //printf("a = %lg b = %lg c = %lg\n", arrayTEST[i].a, arrayTEST[i].b, arrayTEST[i].c);
+        int sum = (int)SolveSquare(arrayTEST[i].a, arrayTEST[i].b, arrayTEST[i].c, &x1, &x2);
 
         //-----verification------//
 
         if (sum != arrayTEST[i].sum){
-            printf("Error on %d test:\nprogram: %d  test:  %lg\n", i, sum, arrayTEST[i].sum);
-            assert(IsZero(sum - arrayTEST[i].sum));
-        };
+            printf("Error on %d test:\nprogram: %d  test:  %d\n", i, sum, arrayTEST[i].sum);
+            assert(IsZero(sum - arrayTEST[i].sum, full_error));
+        }
         if (sum == 1){
-            if (!IsZero(x1 - arrayTEST[i].x1)){
+            if (!IsZero(x1 - arrayTEST[i].x1, full_error)){
                 printf("Error on %d test:\nprogram: %lg  test:  %lg\n", i, x1, arrayTEST[i].x1);
-                assert(IsZero(x1 - arrayTEST[i].x1));
+                assert(IsZero(x1 - arrayTEST[i].x1, full_error));
             }
         }
         if (sum == 2){
-            if (!IsZero(x1 - arrayTEST[i].x1)){
+            if (!IsZero(x1 - arrayTEST[i].x1, full_error)){
                 printf("Error on %d test:\nprogram: %lg  test:  %lg\n", i, x1, arrayTEST[i].x1);
-                assert(IsZero(x1 - arrayTEST[i].x1));
+                assert(IsZero(x1 - arrayTEST[i].x1, full_error));
             }
-            if (!IsZero(x2 - arrayTEST[i].x2)){
+            if (!IsZero(x2 - arrayTEST[i].x2, full_error)){
                 printf("Error on %d test:\nprogram: %lg  test:  %lg\n", i, x2, arrayTEST[i].x2);
-                assert(IsZero(x2 - arrayTEST[i].x2));
+                assert(IsZero(x2 - arrayTEST[i].x2, full_error));
             }
         }
     }
     printf("test : OK");
 }
+#endif
